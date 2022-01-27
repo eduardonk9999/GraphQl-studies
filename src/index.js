4cc0896d6a897c8d2b35a24b252ba57c8e007617
eduardonk9999 <thiago.novak7@gmail.com>
2022-01-27 16:45:24 -0300

    Add, itens

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    author: User!
+  }
   type Query {
     hello: String
+    users: [User!]!
   }
 `;
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello world'
+    hello: () => 'Hello world',
+    users: () => [
+      { _id: String(Math.random()), name: 'Eduardo', email: 'eduardo@gmail.com', active: true},
+      { _id: String(Math.random()), name: 'Eduardo2', email: 'eduardo2@gmail.com', active: true},
+      { _id: String(Math.random()), name: 'Eduardo3', email: 'eduardo3@gmail.com', active: false}
+    ]
   }
-};
+}
+
 
 const server = new ApolloServer({ typeDefs, resolvers });
-
 server.listen().then((url) => console.log(`Server Stardet at ${url}`));
