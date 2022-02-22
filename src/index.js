149a02f38b80ed9a90a33bc3b87100832f461d6a
eduardonk9999 <thiago.novak7@gmail.com>
2022-02-22 18:01:33 -0300

    add, mais conceitos

--- src/index.js
+++ src/index.js
@@ -7,8 +7,21 @@ const { ApolloServer, gql } = require('apollo-server')
 // Mutation -> Manipular dados POST/PUT/DELETE
 
 // Scalater Types -> String, Int, Boolean, Float, ID
+// Entidades -> user, post, produtos
 
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+  
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+  }
   type Query {
     hello: String
   }
@@ -16,8 +29,9 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello World'
-  }  
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => console.log(`Teste Started ${url}`))
+
